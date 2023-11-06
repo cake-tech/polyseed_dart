504d58a5b147fccd3bc85a25f2e72fb32771ddd7
Konstantin Ullrich <konstantinullrich12@gmail.com>
2023-11-06 13:53:50 +0100

    Add comments in example

--- example/polyseed_example.dart
+++ example/polyseed_example.dart
@@ -14,12 +14,12 @@ void main() {
   print('seed2: ${seed2.encode(lang, PolyseedCoin.POLYSEED_MONERO)}');
 
   seed.crypt("CakeWallet");
-  print(seed.isEncrypted);
-  print('seedEnc: ${seed.encode(lang, PolyseedCoin.POLYSEED_MONERO)}');
+  print(seed.isEncrypted); // true
+  print('encrypted: ${seed.encode(lang, PolyseedCoin.POLYSEED_MONERO)}');
 
   seed.crypt("CakeWallet");
-  print(seed.isEncrypted);
-  print('seedEnc: ${seed.encode(lang, PolyseedCoin.POLYSEED_MONERO)}');
+  print(seed.isEncrypted); // false
+  print('decrypted: ${seed.encode(lang, PolyseedCoin.POLYSEED_MONERO)}');
 
   var seedFeatherStr =
       "unaware yard donate shallow slot sing oil oxygen loyal bench near hill surround forum execute lamp";
@@ -27,9 +27,11 @@ void main() {
       Polyseed.decode(seedFeatherStr, lang, PolyseedCoin.POLYSEED_MONERO);
 
   var key = seedFeather.generateKey(PolyseedCoin.POLYSEED_MONERO, 32);
-  print(seedFeather.birthday);
-  print(keyToHexString(key));
-  print(base64.encode(seedFeather.save()));
+  print(seedFeather.birthday); // 1693622412
+  print(keyToHexString(
+      key)); // cbbd142d38347773d44aa830f5f01442aa6d0d3bb48571884479531248e6fa1c
+  print(base64.encode(
+      seedFeather.save())); //UE9MWVNFRUQWAP7QTFMwyWZ55hIVJOa7aluTxzP/Y3c=
 
   var seedLoaded = Polyseed.load(
       base64.decode("UE9MWVNFRUQWAP7QTFMwyWZ55hIVJOa7aluTxzP/Y3c="));
