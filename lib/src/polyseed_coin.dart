abc3e65adfdb5f198503bbf927802a27ef8e44ac
dsc <dsc@xmr.pm>
2023-12-28 08:26:56 +0200

    Add Wownero

--- lib/src/polyseed_coin.dart
+++ lib/src/polyseed_coin.dart
@@ -1,6 +1,7 @@
 enum PolyseedCoin {
   POLYSEED_MONERO,
-  POLYSEED_AEON;
+  POLYSEED_AEON,
+  POLYSEED_WOWNERO;
   // Other coins should be added here sequentially.
   // The maximum supported value is 2047.
   // When adding a new coin, please open a pull request:
